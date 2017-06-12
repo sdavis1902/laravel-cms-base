ea232da11548b6795c69c92c22c3c7d4ed00d331
Scott Davis <s_davis1902@hotmail.com>
2017-06-12 16:17:52 +0000

    test

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,7 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+// master
 MoreRoute::controller('admin/auth', 'AdminAuthController');
 
 Route::get('admin', function(){
